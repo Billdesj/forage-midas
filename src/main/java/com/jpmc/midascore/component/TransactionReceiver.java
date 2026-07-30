94f0742615e0ec46c1b7cc3f5340328b8d415f25
DivineDejaVu <kingbill27@gmail.com>
2026-07-30 02:35:23 -0400

    Complete Task 2 Kafka integration

---
+++ src/main/java/com/jpmc/midascore/component/TransactionReceiver.java
@@ -0,0 +1,14 @@
+package com.jpmc.midascore.component;
+
+import com.jpmc.midascore.foundation.Transaction;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransactionReceiver {
+
+    @KafkaListener(topics = "${general.kafka-topic}")
+    public void receive(Transaction transaction) {
+        float amount = transaction.getAmount();
+    }
+}
